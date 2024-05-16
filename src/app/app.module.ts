a79e49e630028ca239d7942276b4654679594cf9
zhangpan <320311176@qq.com>
2024-05-16 10:54:45 +0800

    阿萨德刚

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,10 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { ProductListComponent } from './product-list/product-list.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ProductListComponent
   ],
   imports: [
     BrowserModule,
