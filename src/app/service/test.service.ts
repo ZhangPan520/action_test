2783ee8b5b2ee37596bfbb95b6086d70ec5ca03c
zhangpan <320311176@qq.com>
2024-05-23 10:22:14 +0800

    侧事故

--- src/app/service/test.service.ts
+++ src/app/service/test.service.ts
@@ -4,10 +4,10 @@ import { HeroListServiceService } from './hero-list-service.service';
 import { Injectable } from '@angular/core';
 
 @Injectable()
-export class TestService extends LogService{
+export class TestService extends LogService {
 
-  constructor(private heroListService:HeroListServiceService) { super()}
-  override log(){
+  constructor(private heroListService: HeroListServiceService) { super() }
+  override log() {
     console.log('test.serssssvice.ts')
     console.log(this.heroListService.getHeroes())
   }
